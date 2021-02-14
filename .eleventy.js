f060cc089a81b866467bfd427160157fc35b20d3
Mike Ralphson <mike.ralphson@gmail.com>
2021-02-14 20:48:39 +0000

    Add seo

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const pluginSEO = require('eleventy-plugin-seo');
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSEO, require("./src/_data/seo.json"));
   // Add a filter using the Config API
   //eleventyConfig.addFilter( "myFilter", function() {});
 
